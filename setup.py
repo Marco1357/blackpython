3c9634c78f57e14246896f427d5512141811cff5
Marco <miranda_garcya@hotmail.com>
2020-06-16 21:42:21 -0300

    Formatodo setup.py

--- setup.py
+++ setup.py
@@ -15,8 +15,8 @@ def read(fname):
 # of replicating them:
 standard_exclude = ["*.py", "*.pyc", "*$py.class", "*~", ".*", "*.bak"]
 standard_exclude_directories = [
-    ".*", "CVS", "_darcs", "./build", "./dist", "EGG-INFO", "*.egg-info"]
-
+    ".*", "CVS", "_darcs", "./build", "./dist", "EGG-INFO", "*.egg-info"
+]
 
 
 # (c) 2005 Ian Bicking and contributors; written for Paste (http://pythonpaste.org)
@@ -34,19 +34,26 @@ def find_package_data(
     """
     Return a dictionary suitable for use in ``package_data``
     in a distutils ``setup.py`` file.
+
     The dictionary looks like::
+
         {"package": [files]}
+
     Where ``files`` is a list of all the files in that package that
     don"t match anything in ``exclude``.
+
     If ``only_in_packages`` is true, then top-level directories that
     are not packages won"t be included (but directories under packages
     will).
+
     Directories matching any pattern in ``exclude_directories`` will
     be ignored; by default directories with leading ``.``, ``CVS``,
     and ``_darcs`` will be ignored.
+
     If ``show_ignored`` is true, then all the files that aren"t
     included in package data are shown on stderr (for debugging
     purposes).
+
     Note patterns use wildcards, or can be exact paths (including
     leading ``./``), and all searching is case-insensitive.
     """
@@ -132,3 +139,4 @@ setup(
     ],
     zip_safe=False,
 )
+
